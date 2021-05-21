4f18bf589e9c2f8a21b10b20521fc4156eed4b05
Haraka Risasi <harakarisasi@gmail.com>
2021-05-22 05:02:55 +0700

    - Комментарии.

---
+++ dart_app_tutorial/bin/dart_app_tutorial.dart
@@ -0,0 +1,9 @@
+var greeting = 'Hello, World!';
+// Hello - is the simple comment.
+
+/// The type of comment for documentation.
+/// example: Please use [greeating] variable to print().
+void main() {
+  print('Hello world!');
+  print(greeting);
+}
