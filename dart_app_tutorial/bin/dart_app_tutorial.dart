f851afc7e81e403b581be47429d322a23a613a11
Haraka Risasi <harakarisasi@gmail.com>
2021-05-29 17:30:59 +0700

    Еще немного про функции.
    - Анонимная функция.
    - Как передавать функцию в другую функцию.

--- dart_app_tutorial/bin/dart_app_tutorial.dart
+++ dart_app_tutorial/bin/dart_app_tutorial.dart
@@ -2,58 +2,22 @@ import 'dart:io';
 
 void main() {
 
-  //FOR
-  // ================================================================
-  var numbers = <int>[1, 2, 34, 5, 632];
+  // Anonymous function
+  (int a, int b) {
+    return a + b;
+  };
+  
 
-  // The FOR loop is used.
-  for (var i in numbers) {
-    var square = i * i;
-    print('$i ^ 2 = $square');
+  // A function can be passed to another funciton.
+  int sum(int a, int b) {
+    return a + b;
   }
 
-  // When you don't need to reassign a variable.
-  for (var i in numbers) {
-    print(i * i);
-  }
+  doWork(sum);
+}
 
-  // The "forEach()"method is used.
-  numbers.forEach((i) {
-    var square = i * i;
+void doWork(int Function(int, int) callback) {
+  var result = callback(1, 2);
 
-    print('$i ^ 2 = $square');
-  });
-
-  // Iteration over the list.
-  for (var i = 0; i < numbers.length; i++) {
-    var element = numbers[i];
-    print(element * i);
-  }
-
-  // While, DoWhile
-  // ================================================================
-  var count = 0;
-
-  while (count < 5) {
-    count++;
-
-    if (count == 3) {
-      count += 17;
-    }
-
-    print(count);
-  }
-
-  while (count < 5) {
-    count++;
-
-    if (count % 2 == 0) continue;
-
-    print(count);
-  }
-  // do {
-  //   count++;
-
-  //   print(count);
-  // } while (count < 5);
+  print(result);
 }
