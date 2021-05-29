85c633f13928bc93c5ea053d298f2555a48ac208
Haraka Risasi <harakarisasi@gmail.com>
2021-05-29 10:56:50 +0700

    FOR, WHILE, DO WHILE
    - вдобавок IF внутри цикла.

--- dart_app_tutorial/bin/dart_app_tutorial.dart
+++ dart_app_tutorial/bin/dart_app_tutorial.dart
@@ -1,23 +1,59 @@
 import 'dart:io';
 
 void main() {
-  // Function in Dart is TRUE OBJECT ORIENTED language.
-  // Function are object that have type >>> Function.
 
-  var fn = inc();
-  fn(); // 1
-  fn(); // 2
-}
+  //FOR
+  // ================================================================
+  var numbers = <int>[1, 2, 34, 5, 632];
 
-
-// The Closure function
-Function inc() {
-  var counter = 0;
-
-  void inner() {
-    counter++;
-    print(counter);
+  // The FOR loop is used.
+  for (var i in numbers) {
+    var square = i * i;
+    print('$i ^ 2 = $square');
   }
 
-  return inner;
+  // When you don't need to reassign a variable.
+  for (var i in numbers) {
+    print(i * i);
+  }
+
+  // The "forEach()"method is used.
+  numbers.forEach((i) {
+    var square = i * i;
+
+    print('$i ^ 2 = $square');
+  });
+
+  // Iteration over the list.
+  for (var i = 0; i < numbers.length; i++) {
+    var element = numbers[i];
+    print(element * i);
+  }
+
+  // While, DoWhile
+  // ================================================================
+  var count = 0;
+
+  while (count < 5) {
+    count++;
+
+    if (count == 3) {
+      count += 17;
+    }
+
+    print(count);
+  }
+
+  while (count < 5) {
+    count++;
+
+    if (count % 2 == 0) continue;
+
+    print(count);
+  }
+  // do {
+  //   count++;
+
+  //   print(count);
+  // } while (count < 5);
 }
