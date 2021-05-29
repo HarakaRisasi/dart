95778dbaef3bf985f9be76cd93f1fa94ad52dfa0
Haraka Risasi <harakarisasi@gmail.com>
2021-05-30 03:22:47 +0700

    OOP - Class and Object.
    Проинициализирован(ы):
        - класс в Dart.
        - методы внутри класса.
        - поля класса.
    
    - late - когда переменной не назначено значение.
    - getArticleInfo() - мой метод, для интерполяции полей класса.
    - Переопределение методов класса внутри объекта класса.
    
    Синтаксический сахар:
    - Каскадная нотация - для последовательных операций над объектом.
    ! вместо стандартного обращения к полям и методам класса.

--- dart_app_tutorial/bin/dart_app_tutorial.dart
+++ dart_app_tutorial/bin/dart_app_tutorial.dart
@@ -1,18 +1,43 @@
-// import 'dart:io';
-
-//TypeDef - is a consise way to refer to a type.
-typedef IntList = List<int>;
-IntList il = [1, 2, 3];
-
-typedef ListMapper<X> = Map<X, List<X>>;
-ListMapper<String> m1 = {
-  'a': ['Hello', '2']
-};
-
 void main() {
-  print(il);
+// Standard work with the object of class.
+// ==========================================================================
+  // The object initializing.
+  var article = Article();
+  // Overriding class methods inside an object
+  article.title = 'Hello, World';
+  article.body = 'This is my code.';
 
-  print(m1['a']);
-  
-  assert(il.length < 2); // False
+  print(article.getArticleInfo());
+
+// ==========================================================================
+  // Cascades (.., ?..) allow you to make a sequence of operations on the same object.
+  var article1 = Article()
+    // cascade notation
+    ..title = 'Hello, Dart'
+    ..body = 'This is my Dart code'
+    ..init1()
+    ..init2();
+
+  print('${article1.title}\n${article1.body}');
+}
+
+class Article {
+  // Class is:
+  // fields - are variables in the class.
+  // getters/setters - are special methods that help change you field values.
+  // methods - are functions in the class.
+  // constructor - this is a class function that is responsible for initializing variables.
+
+// ==========================================================================
+  late String title;
+  late String body;
+
+  String getArticleInfo() => '$title\n$body';
+
+// ==========================================================================
+  void init1() {
+    print(('I\'m method 1'));
+  }
+
+  void init2() => print('I\m method 2');
 }
