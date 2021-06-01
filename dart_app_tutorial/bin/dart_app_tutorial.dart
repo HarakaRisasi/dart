dd9c23cd2f6eb3efd4af636b8f65d594a4dc7946
Haraka Risasi <harakarisasi@gmail.com>
2021-06-01 22:07:39 +0700

    delete branch

--- dart_app_tutorial/bin/dart_app_tutorial.dart
+++ dart_app_tutorial/bin/dart_app_tutorial.dart
@@ -13,7 +13,7 @@ void main() {
   // ================================================
   var article = NewArticle()
     // ..newBody = 'Hellokljhlkjhlkjhklh'; >>> Unhandled exception: Title must be at least 10 characters
-    ..newBody = 'hello, world, and you';
+    ..newBody = 'hello, world, and I.';
 
   // get value from getter;
   print(article.newBody);
